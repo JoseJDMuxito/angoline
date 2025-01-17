34cbd2436a868178559a81d3886497f47e0f4f06
JoseJDMuxito <jose.muxito@etu.univ-lyon1.fr>
2025-01-17 22:20:59 +0100

    dgr

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ themeSwitch.addEventListener('change', () => {
 function applyDarkModeStyles() {
   const customBlocks = document.querySelectorAll('.custom-block');
   customBlocks.forEach((block) => {
-    block.style.backgroundColor = 'rgba(60, 60, 60, 0.9)'; // Ou qualquer cor preferida
+    block.style.backgroundColor = 'rgba(50, 50, 50, 1)'; // Ou qualquer cor preferida
     block.style.color = '#ffffff'; // Exemplo: texto em vermelho
   });
 }
